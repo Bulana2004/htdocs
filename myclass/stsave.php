d285e86541f28bd10042a590b9f6816f684b3e54
Bulana Bimsith <bulana2004@gmail.com>
2024-05-15 10:25:06 +0530

    Update stsave.php 2024/05/15

--- myclass/stsave.php
+++ myclass/stsave.php
@@ -1,11 +1,12 @@
 <?php
-include 'dtbase.php';
+include "dtbase.php";
 
 $val = $_POST["pvals"];
 print_r($val);
 $stid = 110;
-$str1 ="insert into student values($stid,'$val[0]','$val[1]',,'$val[2]',,'$val[3]')";
-$ts1 = $bdd->query($str1) or die("error");
+
+$str1 ="insert into student values($stid,'$val[0]','$val[1]','$val[2]','$val[3]')";
+$ts1 = $bdd->query($str1) or die("error on $str1");
 echo "Complete";
 
 ?>
