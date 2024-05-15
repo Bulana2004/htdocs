248a468b1b25228c53ae4aaa17c61fe9543520e1
Bulana Bimsith <bulana2004@gmail.com>
2024-05-15 20:25:26 +0530

    removing files

--- phplogin/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-
-$db = new PDO ('mysql:host=localhost; dbname=php_login_test; charset=utf8', 'root', '');
-
-}catch(Exception $e){
-    die('Connection error: ' . $e->getMassage() );
-}
-?>
