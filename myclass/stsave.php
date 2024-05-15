c448a0a14be8e7236582c8616d4393d0a8fac52f
Bulana Bimsith <bulana2004@gmail.com>
2024-05-15 09:51:48 +0530

    Update stsave.php

--- myclass/stsave.php
+++ myclass/stsave.php
@@ -1,7 +1,11 @@
 <?php
+include 'dtbase.php';
 
 $val = $_POST["pvals"];
-
 print_r($val);
+$stid = 110;
+$str1 ="insert into student values($stid,'$val[0]','$val[1]',,'$val[2]',,'$val[3]')";
+$ts1 = $bdd->query($str1) or die("error");
+echo "Complete";
 
 ?>
